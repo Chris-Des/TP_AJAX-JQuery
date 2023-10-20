0382c950ee11bb0c566fd12ff7c014ce9fcd7269
Onizuka <desaulty-c@tuta.io>
2023-10-20 13:54:02 +0200

    v1.0.0 : Minimize All

--- TP_AJAX-JQuery/assets/controllers/logout.php
+++ TP_AJAX-JQuery/assets/controllers/logout.php
@@ -1,9 +1,1 @@
-<?php
-session_start(); // Start the session
-
-session_destroy(); // Destroy all session data
-
-header('Location: \Exercice_AJAX-JQuery\TP_AJAX-JQuery\index.php'); // Redirect the user to the specified location
-
-exit(); // Terminate the script execution
-?>
+<?php session_start();session_destroy();header('Location: \Exercice_AJAX-JQuery\TP_AJAX-JQuery\index.php');exit();?>
--- TP_AJAX-JQuery/assets/db/script/createUser.sql
+++ TP_AJAX-JQuery/assets/db/script/createUser.sql
@@ -1,14 +1,2 @@
--- Créer la base de données
-CREATE DATABASE blogsjt;
-
--- Utiliser la base de données
-USE blogsjt;
-
--- Créer la table "user"
-CREATE TABLE user (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  username VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  password_hash VARCHAR(255) NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE DATABASE blogsjt; USE blogsjt; CREATE TABLE user (id INT PRIMARY KEY AUTO_INCREMENT,username VARCHAR(255) NOT NULL,email VARCHAR(255) NOT NULL,password_hash VARCHAR(255) NOT NULL,created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
